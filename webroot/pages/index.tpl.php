85326802757fd77a1afffdadb362c6c144254272
sebcsaba <sebcsaba@gmail.com>
2013-02-17 20:16:31 +0100

    add jquery ui datepicker

--- webroot/pages/index.tpl.php
+++ webroot/pages/index.tpl.php
@@ -2,8 +2,10 @@
 <head>
 <meta http-equiv="Content-type" content="text/html; charset=UTF-8"/>
 <title>GeoReg</title>
+<link rel="stylesheet" href="resources/jquery-ui-1.10.1.custom.min.css"/>
 <link rel="stylesheet" href="resources/style.css" />
 <script type="text/javascript" src="resources/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="resources/jquery-ui-1.10.1.custom.min.js"></script>
 <script type="text/javascript" src="resources/georeg.js"></script>
 </head>
 <body>
