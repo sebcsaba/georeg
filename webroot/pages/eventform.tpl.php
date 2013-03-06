d5a1c89434d5c92c0dd67c99b11951d1ff219856
sebcsaba <sebcsaba@gmail.com>
2013-03-06 20:30:07 +0100

    use translations

--- webroot/pages/eventform.tpl.php
+++ webroot/pages/eventform.tpl.php
@@ -5,23 +5,23 @@
 	<input type="hidden" name="id" value="<?h($event->getId())?>"/>
 	
 	<div>
-		<label for="f_name">Név</label>
+		<label for="f_name"><?h($t('form.event.name'))?></label>
 		<input type="text" id="f_name" name="name" value="<?h($event->getName())?>"/>
 	</div>
 	
 	<div>
-		<label for="f_eventDate">Dátum</label>
+		<label for="f_eventDate"><?h($t('form.event.eventDate'))?></label>
 		<input type="text" id="f_eventDate" name="eventDate" value="<?h($event->getEventDate()->toDayString())?>"/>
 	</div>
 	
 	<div>
-		<label for="f_registrationEnd">Nevezési határidő</label>
+		<label for="f_registrationEnd"><?h($t('form.event.registrationEnd'))?></label>
 		<input type="text" id="f_registrationEnd" name="registrationEnd" value="<?h($event->getRegistrationEnd()->toDayString())?>"/>
 		<input type="text" name="registrationEndTime" value="<?h($event->getRegistrationEnd()->toUserString('H:i'))?>"/>
 	</div>
 	
 	<div>
-		<label for="f_international">Nemzetközi</label>
+		<label for="f_international"><?h($t('form.event.international'))?></label>
 		<input type="checkbox" id="f_international" name="international"
 			<?php if($event->getInternational()) { print('checked="checked"'); } ?> />
 	</div>
--- webroot/pages/leftmenu.tpl.php
+++ webroot/pages/leftmenu.tpl.php
@@ -1,8 +1,8 @@
 <div>Loginfield</div>
 <?php if ($request->getUser()->isAdmin()) { ?>
-<div><a href="javascript:openPage('NewEvent');">Új verseny</a></div>
+<div><a href="javascript:openPage('NewEvent');"><?h($t('left.new_event'))?></a></div>
 <?php } ?>
-<span>Versenyek:</span>
+<span><?h($t('left.events_list'))?></span>
 <ul>
 	<? foreach ($request->getData('events') as $event) { ?>
 		<li><a href="javascript:openPage('Event',{id:<?h($event->getId())?>});"><?h($event->getName())?></a></li>
