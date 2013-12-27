a65fca61916c97c846bc94cf34456dcf414848ff
sebcsaba <sebcsaba@gmail.com>
2013-12-27 16:19:38 +0100

    add config

---
+++ webroot/config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+return config_merge(array(
+
+	'lifecycle' => 'unknown', // values: production, development, test
+	
+	'di' => array(
+		'impl' => array(
+		),
+		'nonsingletons' => array(
+		),
+		'specials' => array(
+			'Database' => new DatabaseDIHandler(),
+		),
+	),
+	
+	'messages' => array(
+		'title.error' => 'Sorry.',
+		'general.error' => 'An error occured. Please contact the site administrator!',
+	),
+	
+	'default.error.page' => 'default.error',
+
+),require_once('config.local.php'));
