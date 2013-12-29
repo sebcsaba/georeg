cba9e5ffa7d74e14c272850fb78d3a4581491e4f
sebcsaba <sebcsaba@gmail.com>
2013-12-29 17:59:34 +0100

    set multilanguage to event application form

--- webroot/pages/event.application.form.tpl.php
+++ webroot/pages/event.application.form.tpl.php
@@ -1,13 +1,22 @@
 <?php $event = $request->getData('event'); ?>
 <div class="eventData">
-<h1><?h($event->getName())?></h1>
-<h2><?h($event->getEventDate()->toDayString())?></h2>
-<?php if ($event->getRegistrationEnd()->isBefore(new Timestamp())) { ?>
-	<div>A regisztráció lezárult.</div>
-<?php } else { ?>
-	<div>Regisztrációs határidő: <?h($event->getRegistrationEnd()->toMinuteString())?></div>
-<?php } ?>
-<?php if ($event->getInternational()) { ?>
-	<div>Ez egy nemzetközi verseny.</div>
-<?php } ?>
+	<h1><?h($event->getName())?></h1>
+	<h2><?h($event->getEventDate()->toDayString())?></h2>
+	
+	<?php if ($event->isRegistrationOpen()) { ?>
+		<div><?h($t('event.application.form.registrationDeadline'))?> <?h($event->getRegistrationEnd()->toMinuteString())?></div>
+	<?php } else { ?>
+		<div><?h($t('event.application.form.registrationClosed'))?></div>
+	<?php } ?>
+	
+	<?php if ($event->getInternational()) { ?>
+		<div><?h($t('event.application.form.internationalEvent'))?></div>
+	<?php } ?>
 </div>
+
+<?php if (count($request->getData('participants'))) { ?>
+	<?php foreach ($request->getData('participants') as $participant) { ?>
+	<?php } ?>
+<?php } else { ?>
+	<div><?h($t('event.application.form.noParticipant'))?></div>
+<?php } ?>
